794accc3cc46291994ed9df8594981f6ae8fdb4f
Elviss Strazdins <elviss@elviss.lv>
2015-12-09 11:56:30 +0200

    Add mouse listener

--- Classes/Editor.cpp
+++ Classes/Editor.cpp
@@ -11,6 +11,8 @@ namespace ParticleEditor
     
     Editor::~Editor()
     {
+        if (_mouseListener) _eventDispatcher->removeEventListener(_mouseListener);
+        
         if (_keybListener) _eventDispatcher->removeEventListener(_keybListener);
     }
 
@@ -21,13 +23,37 @@ namespace ParticleEditor
 			return false;
 		}
         
+        _mouseListener = EventListenerMouse::create();
+        
+        _mouseListener->onMouseDown = CC_CALLBACK_1(Editor::onMouseDown, this);
+        _mouseListener->onMouseUp = CC_CALLBACK_1(Editor::onMouseUp, this);
+        _mouseListener->onMouseMove = CC_CALLBACK_1(Editor::onMouseMove, this);
+        
+        _eventDispatcher->addEventListenerWithSceneGraphPriority(_mouseListener, this);
+        
+        
         _keybListener = EventListenerKeyboard::create();
         _keybListener->onKeyPressed = CC_CALLBACK_2(Editor::onKeyPressed, this);
         
         _eventDispatcher->addEventListenerWithSceneGraphPriority(_keybListener, this);
-		
+        
 		return true;
 	}
+    
+    void Editor::onMouseDown(cocos2d::EventMouse* event)
+    {
+        
+    }
+    
+    void Editor::onMouseUp(cocos2d::EventMouse* event)
+    {
+        
+    }
+    
+    void Editor::onMouseMove(cocos2d::EventMouse* event)
+    {
+        
+    }
     
     void Editor::onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event)
     {
--- Classes/Editor.h
+++ Classes/Editor.h
@@ -18,7 +18,13 @@ namespace ParticleEditor
 	private:
 		DISALLOW_COPY_AND_ASSIGN(Editor);
 		
+        void onMouseDown(cocos2d::EventMouse* event);
+        void onMouseUp(cocos2d::EventMouse* event);
+        void onMouseMove(cocos2d::EventMouse* event);
+        
         void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+        
+        cocos2d::EventListenerMouse* _mouseListener = nullptr;
         
         cocos2d::EventListenerKeyboard* _keybListener = nullptr;
 	};
