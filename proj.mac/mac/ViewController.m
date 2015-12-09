ec59ff0b1bcfd3aa43c6e2ae56507170e3f3d0ef
Elviss Strazdins <elviss@elviss.lv>
2015-12-09 02:30:14 +0200

    Add open and save dialogs

--- proj.mac/mac/ViewController.m
+++ proj.mac/mac/ViewController.m
@@ -26,4 +26,71 @@
     
 }
 
+- (IBAction)handleNew:(id)sender
+{
+    
+}
+
+- (IBAction)handleOpen:(id)sender
+{
+    NSOpenPanel* openPanel = [NSOpenPanel openPanel];
+    
+    openPanel.title = @"Choose a .pef";
+    openPanel.showsResizeIndicator = YES;
+    openPanel.showsHiddenFiles = NO;
+    openPanel.canChooseDirectories = NO;
+    openPanel.canCreateDirectories = YES;
+    openPanel.allowsMultipleSelection = NO;
+    openPanel.allowedFileTypes = @[@"pef"];
+    
+    [openPanel beginWithCompletionHandler:^(NSInteger result) {
+        if (result == NSModalResponseOK)
+        {
+            NSURL* selection = openPanel.URL;
+            
+            [[NSDocumentController sharedDocumentController] noteNewRecentDocumentURL:selection];
+            
+            NSString* path = [selection.path stringByResolvingSymlinksInPath];
+            
+        }
+    }];
+}
+
+- (IBAction)handleSave:(id)sender
+{
+    
+}
+
+- (IBAction)handleSaveAs:(id)sender
+{
+    NSSavePanel* savePanel = [NSSavePanel savePanel];
+    
+    savePanel.title = @"Choose a .pef file";
+    savePanel.showsResizeIndicator = YES;
+    savePanel.showsHiddenFiles = NO;
+    savePanel.canCreateDirectories = YES;
+    savePanel.allowedFileTypes = @[@"pef"];
+    
+    [savePanel beginWithCompletionHandler:^(NSInteger result) {
+        if (result == NSModalResponseOK)
+        {
+            NSURL* selection = savePanel.URL;
+            
+            [[NSDocumentController sharedDocumentController] noteNewRecentDocumentURL:selection];
+            
+            NSString* path = [selection.path stringByResolvingSymlinksInPath];
+        }
+    }];
+}
+
+- (IBAction)handleImport:(id)sender
+{
+    
+}
+
+- (IBAction)handleExport:(id)sender
+{
+    
+}
+
 @end
